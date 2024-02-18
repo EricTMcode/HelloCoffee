dbe12fe09cd1f41151b6fdc4d22d88a43ffca059
Eric <ebagnaro@gmail.com>
2024-02-18 08:18:28 +0100

    Initial Commit

---
+++ HelloCoffee/HelloCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoffeeApp.swift
+//  HelloCoffee
+//
+//  Created by Eric on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
